aa3c7ddd24f3eccabd599b790e8b4496b75fcda1
Gustavo Cunha <gustavocunha@gmail.com>
2013-11-15 18:06:42 -0300

    move apart theme files

---
+++ wp-content/themes/add2013/404.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+<div class="row">
+    <div class="centered twelve columns">
+		<h1>Doh!</h1>
+	</div>
+</div>
+<?php get_footer(); ?>
---
+++ wp-content/themes/add2013/footer.php
@@ -0,0 +1,179 @@
+      <!-- Sponsors -->
+      <section class="sponsors row">
+        <div class="centered eight columns top-page">
+          <h2>Patrocínio</h2>
+          <i></i>
+          <span class="description">As empresas que fizeram o Alagoas Dev Day acontecer</span>
+        </div>
+
+        <div class="clr"></div>
+
+        <div class="row sponsors-list">
+          <div class="two columns cetgory-sponsors offering">Oferecimento</div>
+          <div class="two columns"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/website/sponsors.png" alt=""></div>
+        </div>
+
+        <div class="row sponsors-list">
+          <div class="two columns cetgory-sponsors sponsor">Ouro</div>
+          <div class="two columns"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/website/sponsors.png" alt=""></div>
+          <div class="two columns"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/website/sponsors.png" alt=""></div>
+          <div class="two columns"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/website/sponsors.png" alt=""></div>
+        </div>
+
+        <div class="row sponsors-list">
+          <div class="two columns cetgory-sponsors sponsor">Prata</div>
+          <div class="two columns"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/website/sponsors.png" alt=""></div>
+          <div class="two columns"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/website/sponsors.png" alt=""></div>
+        </div>
+
+        <div class="row sponsors-list">
+          <div class="two columns cetgory-sponsors sponsor">Bronze</div>
+          <div class="two columns"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/website/sponsors.png" alt=""></div>
+          <div class="two columns"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/website/sponsors.png" alt=""></div>
+        </div>
+
+        <div class="row sponsors-list">
+          <div class="two columns cetgory-sponsors">Apoio</div>
+          <div class="two columns"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/website/sponsors.png" alt=""></div>
+          <div class="two columns"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/website/sponsors.png" alt=""></div>
+          <div class="two columns"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/website/sponsors.png" alt=""></div>
+        </div>
+      </section>
+      <!-- End sponsors -->
+
+      <!-- Realization -->
+      <section class="realization">
+        <div class="centered eight columns top-page">
+          <h2>Créditos</h2>
+          <i></i>
+          <span class="description">Estes são os diretores responsáveis pelo ADD</span>
+        </div>
+
+        <div class="row">
+          <div class="centered ten columns">
+            
+            <ul class="organizer-list five_up tiles">
+
+              <li class="organizer-item">
+                <figure class="organizer-photo">
+                  <img class="photo" src="<?php echo get_template_directory_uri(); ?>/assets/images/organizers/carlyson.jpg" alt="Carlyson Oliveira" itemprop="image">
+                </figure>
+
+                <div class="organizer-about">
+                  <h3 class="organizer-title centered eight columns">
+                      Carlyson Oliveira
+                  </h3>
+
+                  <p class="organizer-participation-job">Organizador | Designer</p>
+                
+                  <ul class="organizer-social two_up tiles">
+                    <li><a href="https://twitter.com/Carlyson" title="Twitter" class="icon-twitter" target="_blank"></a></li>
+                    <li><a href="http://www.linkedin.com/in/carlyson" title="Linkedin" class="icon-linkedin" target="_blank"></a></li>
+                  </ul>
+                </div>
+              </li>
+
+              <li class="organizer-item">
+                <figure class="organizer-photo">
+                  <img class="photo" src="<?php echo get_template_directory_uri(); ?>/assets/images/organizers/gustavo.jpg" alt="Gustavo Cunha" itemprop="image">
+                </figure>
+
+                <div class="organizer-about">
+                  <h3 class="organizer-title centered eight columns">
+                      Gustavo Cunha
+                  </h3>
+
+                  <p class="organizer-participation-job">Organizador | Back-end</p>
+                
+                  <ul class="organizer-social two_up tiles">
+                    <li><a href="https://twitter.com/gmmcal" title="Twitter" class="icon-twitter" target="_blank"></a></li>
+                    <li><a href="http://www.linkedin.com/in/gmmcal" title="Linkedin" class="icon-linkedin" target="_blank"></a></li>
+                  </ul>
+                </div>
+              </li>
+
+              <li class="organizer-item">
+                <figure class="organizer-photo">
+                  <img class="photo" src="<?php echo get_template_directory_uri(); ?>/assets/images/organizers/hugo.jpg" alt="Hugo Oliveira" itemprop="image">
+                </figure>
+
+                <div class="organizer-about">
+                  <h3 class="organizer-title centered eight columns">
+                      Hugo Oliveira
+                  </h3>
+
+                  <p class="organizer-participation-job">Organizador | Conteúdo</p>
+                
+                  <ul class="organizer-social two_up tiles">
+                    <li><a href="https://twitter.com/oliveira_hugo" title="Twitter" class="icon-twitter" target="_blank"></a></li>
+                    <li><a href="http://www.linkedin.com/in/hugooliveira12" title="Linkedin" class="icon-linkedin" target="_blank"></a></li>
+                  </ul>
+                </div>
+              </li>
+
+              <li class="organizer-item">
+                <figure class="organizer-photo">
+                  <img class="photo" src="<?php echo get_template_directory_uri(); ?>/assets/images/organizers/ivan.jpg" alt="Ivan Santos" itemprop="image">
+                </figure>
+
+                <div class="organizer-about">
+                  <h3 class="organizer-title centered eight columns">
+                      Ivan Santos
+                  </h3>
+
+                  <p class="organizer-participation-job">Organizador | Front-end</p>
+                
+                  <ul class="organizer-social two_up tiles">
+                    <li><a href="https://twitter.com/pragmaticivan" title="Twitter" class="icon-twitter" target="_blank"></a></li>
+                    <li><a href="http://www.linkedin.com/in/pragmaticivan" title="Linkedin" class="icon-linkedin" target="_blank"></a></li>
+                  </ul>
+                </div>
+              </li>
+
+              <li class="organizer-item">
+                <figure class="organizer-photo">
+                  <img class="photo" src="<?php echo get_template_directory_uri(); ?>/assets/images/organizers/mac.jpg" alt="Maclévison Gyovanni" itemprop="image">
+                </figure>
+
+                <div class="organizer-about">
+                  <h3 class="organizer-title centered eight columns">
+                      Mac Gyovanni
+                  </h3>
+
+                  <p class="organizer-participation-job">Organizador | Front-end</p>
+                
+                  <ul class="organizer-social two_up tiles">
+                    <li><a href="https://twitter.com/maclevison" title="Twitter" class="icon-twitter" target="_blank"></a></li>
+                    <li><a href="http://www.linkedin.com/in/maclevison" title="Linkedin" class="icon-linkedin" target="_blank"></a></li>
+                  </ul>
+                </div>
+              </li>
+
+            </ul>
+          </div>
+        </div>
+      </section>
+      <!-- End Realization -->
+      
+      <footer>
+        <div class="row">
+          <div class="twelve columns" itemscope itemtype="http://schema.org/LocalBusiness">
+            <span>Quer falar com a gente? Manda ver!</span>
+            <a href="callto:+558233177586" itemprop="telephone">82 3317.7586</a> | <a href="mailto:contato@alagoasdevday.com.br">contato@alagoasdevday.com.br</a> 
+          </div>
+        </div>
+      </footer>
+    </div>
+    <!-- End container Main -->
+    <?php wp_footer(); ?>
+
+    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+    <script>
+    var _gaq=[['_setAccount','UA-22513742-22'],['_trackPageview']];
+    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g,s)}(document,'script'));
+    </script>
+
+</body>
+</html>
