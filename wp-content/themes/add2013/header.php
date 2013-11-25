027493186144ef0ed259452ccadceeb48b5189f5
Maclevison Gyovanni <mac@zeropixel.com.br>
2013-11-25 18:31:29 -0300

    Fix urls icons

--- wp-content/themes/add2013/header.php
+++ wp-content/themes/add2013/header.php
@@ -18,14 +18,13 @@
   <meta property="og:url" content="http://alagoasdevday.com.br">
   <meta property="og:title" content="Alagoas Dev Day">
   <meta property="og:description" content="Juntamos o visual de uma cidade paradisíaca, um lugar que respira arte e separamos um dia para conversar apenas sobre programação. O resultado disso tudo é uma experiência incrível sobre desenvolvimento em diversas linguagens.">
-  <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/assets/images/icons/facebook.jpg">
+  <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/assets/images/icon/facebook.jpg">
 
-  <!-- Place favicon.ico and apple-touch-icon.png in the <?php echo get_template_directory_uri(); ?>/assets/images/icons directory -->
-  <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/icons/favicon.ico" type="image/x-icon">
-  <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/icons/apple-touch-icon.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/assets/images/icons/apple-touch-icon-72x72.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/assets/images/icons/apple-touch-icon-114x114.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/assets/images/icons/apple-touch-icon-144x144.png">
+  <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/icon/favicon.ico" type="image/x-icon">
+  <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/icon/apple-touch-icon.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/assets/images/icon/apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/assets/images/icon/apple-touch-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/assets/images/icon/apple-touch-icon-144x144.png">
   <?php wp_head(); ?>
 </head>
 <body>
